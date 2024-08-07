b9cc0fdb466d3055d2b79fb3800a803ddf23e376
adila <nradilaaa@gmail.com>
2024-08-07 08:00:28 +0800

    accessibility

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-    // Initialize Slick Carousel
-    $('.news-carousel').slick({
-        dots: true,
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 1,
-        adaptiveHeight: true
-    });
 
     // Burger Menu Toggle
     $('.burger').click(function(){
@@ -18,17 +10,4 @@ $(document).ready(function(){
         var img = $(this);
         img.attr('src', img.data('src'));
     });
-
-    var configProfile = {
-        "profile": {"screenName": 'TwitterDev'},
-        "domId": 'twitter-feed',
-        "maxTweets": 5,
-        "enableLinks": true, 
-        "showUser": true,
-        "showTime": true,
-        "showImages": true,
-        "lang": 'en'
-    };
-    
-    twitterFetcher.fetch(configProfile);
 });
